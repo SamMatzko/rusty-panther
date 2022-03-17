e1557d5b47476d339e1642c1442f4c0f5316cf06
Samuel Matzko <matzko+sam@gmail.com>
2022-03-17 19:11:44 -0400

    🐛🎨 Bypassed the bug in the previous commit, renamed theme.rs to themes.rs for more simplicity in keeping variable names separate.

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,11 @@
 //! terminal manipulation library. Once more code gets written, documentation and
 //! examples will appear here.
 
-pub mod theme;
+pub mod themes;
 pub mod widgets;
-
-use std::io::Write;
 
 /// The struct that runs and handles everything. Will have one main window, and any
 /// amount of sub-windows.
-pub struct Application<W: Write> {
-    pub window: widgets::Window<W>,
+pub struct Application {
+    pub window: widgets::Window,
 }
--- src/widgets.rs
+++ src/widgets.rs
@@ -1,30 +1,37 @@
 //! The module that contains all the widgets used in creating GUIs.
 
-use crate::theme;
+use crate::themes;
 
-use std::io::{stdout, Write};
+use std::io::stdout;
 use termion;
-use termion::raw::IntoRawMode;
+use termion::raw::{IntoRawMode, RawTerminal};
 use termion::screen::*;
 
-// termion::screen::AlternateScreen<std::io::stdio::Stdout>
-
-pub struct Window<W: Write> {
-    screen: AlternateScreen<W>,
-    stdout: termion::raw::RawTerminal<std::io::Stdout>,
+pub struct Window {
+    stdout: RawTerminal<std::io::Stdout>,
+    theme: themes::Theme,
 }
-impl<W: Write> Window<W> {
+impl Window {
     
-    /// Returns the completed [`Window`]; to be called after all the builder functions
-    pub fn build(self) -> Window<W> {
-        Window { screen: self.screen, stdout: self.stdout }
+    /// Returns the completed [`Window`]; to be called after all the builder functions.
+    /// This function also creates the [`AlternateScreen`].
+    pub fn build(self) -> Window {
+        AlternateScreen::from(stdout());
+        Window { stdout: self.stdout, theme: self.theme }
     }
 
     /// Returns a new [`Window`], which can then be built using the builder functions
-    pub fn builder() -> Window<W> {
+    pub fn builder() -> Window {
         let out = stdout().into_raw_mode().unwrap();
-        Window { screen: AlternateScreen::from(out), stdout: out, }
+        Window { stdout: out, theme: themes::default() }
     }
 
-    // The builder functions. These 
+    // The builder functions. These can be used to optionally customize options.
+    // Be sure to call [`build()`] to finalize the creation.
+
+    /// Set the theme for the window.
+    pub fn set_theme(&mut self, theme: themes::Theme) -> &mut Window {
+        self.theme = theme;
+        self
+    }
 }
