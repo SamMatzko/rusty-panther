1981003d5de22f65e4aea15863ebc4c604a2f357
Samuel Matzko <matzko+sam@gmail.com>
2022-03-30 16:43:03 -0400

    🚧 Started a Grid struct which will handle all sizing-related stuff for parent widgets.

--- src/structure.rs
+++ src/structure.rs
@@ -4,6 +4,162 @@
 use crate::traits::Buildable;
 use crossterm::style::Color;
 
+/// This struct contains sizing data used in gridding widgets, including how many
+/// rows/columns a parent widget has, and how much of the grid those rows/columns
+/// take up.
+/// 
+/// Default width×height is 5×5.
+pub struct Grid {
+    /// A [`Vec<GridColumn>`] containing all of this grid's columns
+    pub columns: Vec<GridColumn>,
+    /// A [`Vec<GridRow>`] containing all of this grid's rows
+    pub rows: Vec<GridRow>,
+    height_: u8,
+    width_: u8,
+}
+impl Grid {
+
+    /// Configure the size of a particular column, and set its priority to [`true`]
+    pub fn column_configure(&self, col: usize, percent: u8) {
+        self.columns[col] = GridColumn(percent, true);
+        self.recalculate();
+    }
+
+    /// Recalculate the size of all the rows and columns based on which ones have
+    /// user-set percentates.
+    pub fn recalculate(&self) {
+
+        // First calculate the rows, giving prioritized rows the priority
+
+        /* Loop over all the rows, subtracting the prioritized rows' percent from
+        the available room percentage, and subtracting `1` from the number of
+        total rows. This leaves us with the percent that the unprioritized rows
+        will take up (`row_p`), and the number of unprioritized rows there are
+        (`rows`)
+        */
+        let mut row_p = 100;
+        let mut rows = self.rows.len();
+        for row in &self.rows {
+            if row.1 {
+                row_p -= row.0;
+                rows -= 1;
+            }
+        }
+
+        // Now go through all the UNprioritized rows and divide the remaing
+        // percent up between them
+        let percent_for_rows = row_p / rows as u8;
+        let mut i = 0;
+        for row in &self.rows {
+            if !row.1 {
+                self.rows[i] = GridRow(percent_for_rows, false);
+            }
+            i += 1;
+        }
+
+        // Now calculate the columns, giving prioritized columns the priority
+
+        /* Loop over all the columns, subtracting the prioritized columns' percent from
+        the available room percentage, and subtracting `1` from the number of
+        total columns. This leaves us with the percent that the unprioritized columns
+        will take up (`column_p`), and the number of unprioritized columns there are
+        (`columns`)
+        */
+        let mut column_p = 100;
+        let mut columns = self.columns.len();
+        for column in &self.columns {
+            if column.1 {
+                column_p -= column.0;
+                columns -= 1;
+            }
+        }
+
+        // Now go through all the UNprioritized columns and divide the remaing
+        // percent up between them
+        let percent_for_columns = column_p / columns as u8;
+        let mut i = 0;
+        for column in &self.columns {
+            if !column.1 {
+                self.columns[i] = GridColumn(percent_for_columns, false);
+            }
+            i += 1;
+        }
+    }
+
+    /// Configure the size of a particular row, and set its priority to [`true`]
+    pub fn row_configure(&self, row: usize, percent: u8) {
+        self.rows[row] = GridRow(percent, true);
+        self.recalculate();
+    }
+
+    // These methods are the builder-pattern methods; they need to be called in
+    // between `builder()` and `build()`
+    
+    /// Set the height of the grid, in rows
+    pub fn height(mut self, height: u8) -> Grid {
+
+        // Set the height
+        self.height_ = height;
+
+        // Re-configure the list of rows based on the height given, calculating
+        // the new row-size percent
+        let percent: u8 = 100 / self.height_;
+        self.rows = Vec::new();
+        for _ in 0..self.height_ { self.rows.push(GridRow(percent, false)) }
+        self
+    }
+
+    /// Set the width of the grid, in columns
+    pub fn width(mut self, width: u8) -> Grid {
+
+        // Set the width
+        self.width_ = width;
+
+        // Re-configure the list of columns based on the width given, calculating
+        // the new column-size percent
+        let percent: u8 = 100 / self.width_;
+        self.columns = Vec::new();
+        for _ in 0..self.width_ { self.columns.push(GridColumn(percent, false)) }
+        self
+    }
+}
+impl Buildable for Grid {
+
+    fn build(self) -> Grid {
+        Grid { columns: self.columns, rows: self.rows, height_: self.height_, width_: self.width_ }
+    }
+
+    fn builder() -> Grid {
+        let col = GridColumn(20, false);
+        let row = GridRow(20, false);
+        Grid {
+            columns: vec![col, col, col, col, col],
+            rows: vec![row, row, row, row, row],
+            height_: 5,
+            width_: 5,
+        }
+    }
+
+    fn new() -> Grid {
+        Grid::builder().build()
+    }
+}
+
+/// The struct for storing a grid column's data.
+/// 
+/// The [`u8`] is the percentage of the grid's width that this column will take
+/// up. The [`bool`] tells whether this column's size should be given a priority
+/// or not.
+pub struct GridColumn(u8, bool);
+
+/// The struct for storing a grid row's data
+/// 
+/// The [`u8`] is the percentage of the grid's height that this row will take
+/// up. The [`bool`] tells whether this row's size should be given a priority
+/// or not.
+pub struct GridRow(u8, bool);
+
+/// The struct used for creating and setting widget themes.
 pub struct Theme {
     pub fg: (u8, u8, u8),
     pub bg: (u8, u8, u8),
